4dcfade3bcddb85482116467b1c456ff0c6e85f8
Abdul-Wahab <ogbrutal2825@gmail.com>
2026-07-25 19:59:25 +0500

    Server Check Done & Home and About apis are Created.

---
+++ main.py
@@ -0,0 +1,27 @@
+from ast import Lambda
+
+from fastapi import FastAPI,Path,HTTPException, Query, responses
+import json 
+from fastapi.responses import JSONResponse
+
+
+app = FastAPI()
+
+def loadData():
+    with open("patients.json") as f:
+        data = json.load(f)
+    return data
+
+def saveData(data):
+    with open("patients.json","w") as f:
+        json.dump(data,f)
+
+
+
+@app.get("/",tags=["Health"])
+def hello_world_api():
+    return {"message": "Hello, World!"}
+
+@app.get("/about",tags=["Health"])
+def about():
+    return {"message":" Hey i am learning fastapi and i am enjoying it !!!"}
